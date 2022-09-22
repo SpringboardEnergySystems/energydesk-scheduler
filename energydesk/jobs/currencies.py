98875105effce068ac4a4bb93b826449dc3a9e46
MortenBirkeland <morteb1507@gmail.com>
2022-09-22 14:37:28 +0200

    api scheduler

--- energydesk/jobs/currencies.py
+++ energydesk/jobs/currencies.py
@@ -1,5 +1,19 @@
 import logging
+from energydeskapi.sdk.common_utils import load_env
+from energydeskapi.sdk.api_connection import ApiConnection
+from energydeskapi.scheduler.scheduler_api import SchedulerApi
+import environ
+import requests
 logger = logging.getLogger(__name__)
 
 def download_currencydata():
+    env = environ.Env()
+    tok = env.str('ENERGYDESK_TOKEN')
+    url = env.str('ENERGYDESK_URL')
+    headers = {'Authorization': "token" + ' ' + tok}
+    payload = {}
+    result = requests.post(url + "/api/currencies/download-currencydata/", json=payload, headers=headers)
+    # api_conn = ApiConnection(url)
+    # api_conn.set_token(tok, "Token")
+    print(result.text)
     logger.info("Downloading currency data")
