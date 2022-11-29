22396bfe3ac2d7ea8c1b8260f8fdb900c64b4f87
MortenBirkeland <morteb1507@gmail.com>
2022-11-29 10:11:27 +0100

    baas schedules

---
+++ energydesk/jobs/baas.py
@@ -0,0 +1,33 @@
+import logging
+from energydeskapi.sdk.common_utils import load_env
+from energydeskapi.sdk.api_connection import ApiConnection
+from energydeskapi.scheduler.scheduler_api import SchedulerApi
+import environ
+import requests
+logger = logging.getLogger(__name__)
+
+def check_scheduled_dispatches():
+    env = environ.Env()
+    tok = env.str('ENERGYDESK_TOKEN')
+    url = env.str('ENERGYDESK_URL')
+    headers = {'Authorization': "token" + ' ' + tok}
+    payload = {}
+    full_url=url + "/api/baas/exec-baas-check-scheduled-dispatches/"
+    result = requests.post(full_url, json=payload, headers=headers)
+    # api_conn = ApiConnection(url)
+    # api_conn.set_token(tok, "Token")
+    print("\nResult", result.status_code, full_url)
+    logger.info("Checking scheduled dispatches")
+
+def optimize_battery():
+    env = environ.Env()
+    tok = env.str('ENERGYDESK_TOKEN')
+    url = env.str('ENERGYDESK_URL')
+    headers = {'Authorization': "token" + ' ' + tok}
+    payload = {}
+    full_url=url + "/api/baas/exec-baas-optimize-battery/"
+    result = requests.post(full_url, json=payload, headers=headers)
+    # api_conn = ApiConnection(url)
+    # api_conn.set_token(tok, "Token")
+    print("\nResult", result.status_code, full_url)
+    logger.info("Optimizing battery")
--- energydesk/jobs/baseliines.py
+++
@@ -1,5 +0,0 @@
-import logging
-logger = logging.getLogger(__name__)
-
-def exec_job_generate_baselines():
-    logger.info("Generating baselines")
