d66bada37c621b3208364f566b08fe3089c9ee2f
MortenBirkeland <morteb1507@gmail.com>
2022-12-21 08:54:56 +0100

    baas schedules

--- energydesk/jobs/flexoptimizer.py
+++ energydesk/jobs/flexoptimizer.py
@@ -13,11 +13,11 @@ def check_scheduled_dispatches():
     headers = {'Authorization': "token" + ' ' + tok}
     payload = {}
     full_url=url + "/api/flexoptimizer/exec-check-scheduled-dispatches/"
-    result = requests.post(full_url, json=payload, headers=headers)
-    # api_conn = ApiConnection(url)
-    # api_conn.set_token(tok, "Token")
-    print("\nResult", result.status_code, full_url)
-    logger.info("Checking scheduled dispatches")
+    # result = requests.post(full_url, json=payload, headers=headers)
+    # # api_conn = ApiConnection(url)
+    # # api_conn.set_token(tok, "Token")
+    # print("\nResult", result.status_code, full_url)
+    # logger.info("Checking scheduled dispatches")
 
 def optimize_battery():
     env = environ.Env()
@@ -26,8 +26,8 @@ def optimize_battery():
     headers = {'Authorization': "token" + ' ' + tok}
     payload = {}
     full_url=url + "/api/flexoptimizer/exec-optimize-battery/"
-    result = requests.post(full_url, json=payload, headers=headers)
-    # api_conn = ApiConnection(url)
-    # api_conn.set_token(tok, "Token")
-    print("\nResult", result.status_code, full_url)
-    logger.info("Optimizing battery")
+    # result = requests.post(full_url, json=payload, headers=headers)
+    # # api_conn = ApiConnection(url)
+    # # api_conn.set_token(tok, "Token")
+    # print("\nResult", result.status_code, full_url)
+    # logger.info("Optimizing battery")
