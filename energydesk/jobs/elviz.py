69d34ee381658a10ba87a79c0d8c3a6dd6e5316e
MortenBirkeland <morteb1507@gmail.com>
2023-02-10 11:58:07 +0100

    elviz schedules

--- energydesk/jobs/elviz.py
+++ energydesk/jobs/elviz.py
@@ -32,32 +32,10 @@ def load_elviz_trades():
             print(json_res)
             market_product_key = json_res[0]['pk']
         print(t)
-        contract = ApiContract()
-        contract.external_contract_id = t['external_contract_id']
-        contract.trade_date = t['trade_date']
-        contract.trade_datetime = t['trade_time']
-        contract.trading_book = t['trading_book']
-        contract.trader = t['trader']
-        contract.contract_price = FormattedMoney(t['contract_price']['amount'], t['contract_price']['currency'])
-        contract.quantity = t['quantity']
-        contract.trading_fee = FormattedMoney(t['trading_fee']['amount'], t['trading_fee']['currency'])
-        contract.clearing_fee = FormattedMoney(t['clearing_fee']['amount'], t['clearing_fee']['currency'])
-        contract.commodity_type = t['commodity']['commodity_type']
-        contract.instrument_type = InstrumentTypeEnum(t['commodity']['instrument_type'])
-        contract.contract_status = t['contract_status']
-        contract.buy_or_sell = t['buy_or_sell']
-        contract.counterpart = t['counterpart']
-        contract.market = MarketEnum(t['commodity']['market'])
-        contract.marketplace_product = market_product_key
-        contract.contract_status = t['contract_status']
-        contract.commodity_profile = t['commodity']['commodity_profile']
-        contract.delivery_type = t['commodity']['delivery_type']
-        contract.commodity_delivery_from = t['commodity']['delivery_from']
-        contract.commodity_delivery_until = t['commodity']['delivery_until']
-        contract.product_code = t['commodity']['product_code']
-        contract.contract_tags = t['contract_tags']
-        print(contract.get_dict(api_conn))
-        contracts.append(contract)
+
+        contract_obj = ApiContract.from_simple_dict(t)
+        print(contract_obj.get_dict(api_conn))
+        contracts.append(contract_obj)
     print(contracts)
     full_url = proxy_url + "/api/elviztrades"
     print("\nResult", full_url)
